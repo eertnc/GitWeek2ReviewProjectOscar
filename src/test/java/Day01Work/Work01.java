bea46d088a6db6814983d7c4f04ad841e8a594f8
melisabeyazrt <kabaraciyanmelisa@gmail.com>
2021-12-26 19:23:32 +0000

    day1work1 is done

--- src/test/java/Day01Work/Work01.java
+++ src/test/java/Day01Work/Work01.java
@@ -1,6 +1,6 @@
 package Day01Work;
 
 public class Work01 {
-
+//work1 is done
 
 }
---
+++ src/test/java/MelisaWork/MelisaWorkDay1.java
@@ -0,0 +1,9 @@
+package MelisaWork;
+
+public class MelisaWorkDay1 {
+
+    public static void main(String[] args) {
+
+        //day 1 work1 is done
+    }
+}
